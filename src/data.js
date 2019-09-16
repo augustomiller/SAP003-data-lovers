81b7e05572493d1c6410284ba9f4d9a1dd145c88
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-09-16 11:54:32 -0300

    Correção de espaço

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
-function getSpecies(specie){
+function getSpecies(specie) {
   const result = RICKANDMORTY.results.filter( person => person.species === specie);
   return result;
 }
-function getGender(gender){
+function getGender(gender) {
   const result = RICKANDMORTY.results.filter( person => person.gender === gender);
   return result;
 }
-function getStatus(status){
+function getStatus(status) {
   const result = RICKANDMORTY.results.filter( person => person.status === status);
   return result;
 }
-function getName(name){
+function getName(name) {
   const result = RICKANDMORTY.results.filter (person => person.name === name);
   return result;
 }
