3f0ce2412ab03f9eed8c84ba460ec9fb2f7bd966
alinechaves <line.limachaves@gmail.com>
2019-09-29 13:13:49 -0300

    criação do grafico

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ const menuStatus = document.getElementById("menuStatus");
 const menuGender = document.getElementById("menuGender");
 const card = document.getElementById("cards");
 const menuOrder = document.getElementById("menuOrder");
+const valorStatistic = document.getElementById("Statistics")//grafico
+
+valorStatistic.addEventListener("change",grafico); //grafico
 
 menuSpecie.addEventListener("change", () =>{
   buildCard(app.filterData(data, menuSpecie.value, "species"));
@@ -71,4 +74,37 @@ function porcentagemStatus(status) {
   statistic.innerHTML =`<p> This status represents ${calculo}% of the characters in the cartoon.</p>`;
 
 }
+
+//grafico 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(grafico);
+
+function grafico (){
+  if(valorStatistic.value === "male&Female"){
+  let dados = google.visualization.arrayToDataTable([
+    ['Gênero','Valor'],
+    ['Female', (app.filterData(data, "Female", "gender").length)] //coluna do grafico
+    ['Male', (app.filterData(data, "Male", "gender").length)]
+  ]);
+  let options = {'title':'Grafico de gênero dos personagens de Rick and Morty',
+                   'width':400,
+                   'height':300};
+  const chart = new google.visualization.ColumnChart("graphic") //div que irá receber 
+  chart.draw(dados,options);
+  }
+  if (valorStatistic.value === "human&Alien"){
+    let dados = google.visualization.arrayToDataTable([
+      ['Specie','Valor'],
+      ['Human', (app.filterData(data, "Human", "species").length)]
+      ['Alien', (app.filterData(data, "Alien", "species").length)]
+    ]);
+    let options = {'title':'Grafico de gênero dos personagens de Rick and Morty',
+                     'width':400,
+                     'height':300};
+    const chart = new google.visualization.ColumnChart("graphic")
+    chart.draw(dados,options); 
+  }
+}
+
+
 
