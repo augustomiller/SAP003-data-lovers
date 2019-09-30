a4d9dac586540b4c4243066a5ef9408f095d3bad
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-09-30 01:19:45 -0500

    Gráficos responsivos e limpando os cards

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ const menuStatus = document.getElementById("menuStatus");
 const menuGender = document.getElementById("menuGender");
 const card = document.getElementById("cards");
 const menuOrder = document.getElementById("menuOrder");
-const valorStatistic = document.getElementById("Statistics");//grafico
+const valorStatistic = document.getElementById("Statistics"); //grafico
+const graphic = document.getElementById("graphic");
 
-menuSpecie.addEventListener("change", () =>{
+menuSpecie.addEventListener("change", () => {
   buildCard(app.filterData(data, menuSpecie.value, "species"));
-  porcentagemEspecie (menuSpecie.value);
-}
-);
+  porcentagemEspecie(menuSpecie.value);
+});
 
-menuStatus.addEventListener("change", () =>{
+menuStatus.addEventListener("change", () => {
   buildCard(app.filterData(data, menuStatus.value, "status"));
   porcentagemStatus(menuStatus.value);
-}
-);
-menuGender.addEventListener("change", () =>{
+});
+menuGender.addEventListener("change", () => {
   buildCard(app.filterData(data, menuGender.value, "gender"));
   porcentagemGenero(menuGender.value);
 });
 menuOrder.addEventListener("change", () => {
-  buildCard(app.selectOrderAlpha(menuOrder.value, data));});
+  buildCard(app.selectOrderAlpha(menuOrder.value, data));
+});
 
 valorStatistic.addEventListener("change", grafico); //grafico
 
@@ -43,88 +43,84 @@ function buildCard(app) {
          Location: ${person.location.name} </p>
         </div>`;
   });
+  graphic.innerHTML = "";
   card.innerHTML = layout;
-
 }
 
 const statistic = document.getElementById("percentage");
 
 function porcentagemEspecie(specie) {
-  let totalSpecie = (data.length);
+  let totalSpecie = data.length;
   let especie = app.filterData(data, specie, "species").length;
-  let calculo = ((especie/totalSpecie)*100).toFixed(2);
+  let calculo = ((especie / totalSpecie) * 100).toFixed(2);
 
-  statistic.innerHTML =`<p> This species represents ${calculo}% of the characters in the cartoon.</p>`;
-
+  statistic.innerHTML = `<p> This species represents ${calculo}% of the characters in the cartoon.</p>`;
 }
 
 function porcentagemGenero(gender) {
-  let totalGender = (data.length);
+  let totalGender = data.length;
   let genero = app.filterData(data, gender, "gender").length;
-  let calculo = ((genero/totalGender)*100).toFixed(2);
+  let calculo = ((genero / totalGender) * 100).toFixed(2);
 
-  statistic.innerHTML =`<p> This gender represents ${calculo}% of the characters in the cartoon.</p>`;
-
+  statistic.innerHTML = `<p> This gender represents ${calculo}% of the characters in the cartoon.</p>`;
 }
 
 function porcentagemStatus(status) {
-  let totalStatus = (data.length);
+  let totalStatus = data.length;
   let valorStatus = app.filterData(data, status, "status").length;
-  let calculo = ((valorStatus/totalStatus)*100).toFixed(2);
+  let calculo = ((valorStatus / totalStatus) * 100).toFixed(2);
 
-  statistic.innerHTML =`<p> This status represents ${calculo}% of the characters in the cartoon.</p>`;
-
+  statistic.innerHTML = `<p> This status represents ${calculo}% of the characters in the cartoon.</p>`;
 }
-google.charts.load("current", {"packages": ["corechart"]});
+google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(grafico);
 
-function grafico () {
+function grafico() {
   if (valorStatistic.value === "male&Female") {
     let dados = new google.visualization.DataTable();
     dados.addColumn("string", "Topping");
     dados.addColumn("number", "Quantidade");
     dados.addRows([
-      ["Female", (app.filterData(data,"Female","gender")).length],
-      ["Male", (app.filterData(data,"Male","gender")).length]
+      ["Female", app.filterData(data, "Female", "gender").length],
+      ["Male", app.filterData(data, "Male", "gender").length]
     ]);
-    let options = {"title": "Grafico de Gênero dos personagens de Rick and Morty",
-    "width": 400,
-    "height": 400
+    let options = {
+      title: "Grafico de Gênero dos personagens de Rick and Morty",
+      height: 400
     };
-    const chart = new google.visualization.ColumnChart(document.getElementById("graphic"));
+    const chart = new google.visualization.ColumnChart(graphic);
     chart.draw(dados, options);
-    card.innerHTML = ""
-  }
-  else if (valorStatistic.value === "human&Alien") {
+    card.innerHTML = "";
+  } else if (valorStatistic.value === "human&Alien") {
     let dados = new google.visualization.DataTable();
     dados.addColumn("string", "Topping");
     dados.addColumn("number", "Quantidade");
     dados.addRows([
-      ["Humano", (app.filterData(data,"Human","species")).length],
-      ["Alien", (app.filterData(data,"Alien","species")).length]
+      ["Humano", app.filterData(data, "Human", "species").length],
+      ["Alien", app.filterData(data, "Alien", "species").length]
     ]);
-    let options = {"title": "Grafico de Species dos personagens de Rick and Morty",
-    "width": 400,
-    "height": 400
+    let options = {
+      title: "Grafico de Species dos personagens de Rick and Morty",
+      height: 400
     };
-    const chart = new google.visualization.ColumnChart(document.getElementById("graphic"));
+    const chart = new google.visualization.ColumnChart(graphic);
     chart.draw(dados, options);
-    card.innerHTML = ""
+    card.innerHTML = "";
   }
-  if(valorStatistic.value === "alive&Dead") {
+  if (valorStatistic.value === "alive&Dead") {
     let dados = new google.visualization.DataTable();
     dados.addColumn("string", "Topping");
     dados.addColumn("number", "Quantidade");
     dados.addRows([
-      ["Vivo", (app.filterData(data,"Alive","status")).length],
-      ["Morto", (app.filterData(data,"Dead","status")).length]
+      ["Vivo", app.filterData(data, "Alive", "status").length],
+      ["Morto", app.filterData(data, "Dead", "status").length]
     ]);
-    let options = {"title": "Grafico de Status de vida dos personagens de Rick and Morty",
-      "width": 400,
-      "height": 400
+    let options = {
+      title: "Grafico de Status de vida dos personagens de Rick and Morty",
+      height: 400
     };
-    const chart = new google.visualization.ColumnChart(document.getElementById("graphic")); 
+    const chart = new google.visualization.ColumnChart(graphic);
     chart.draw(dados, options);
-    card.innerHTML = ""
+    card.innerHTML = "";
   }
 }
